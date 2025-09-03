8583e5479f4e6802efbd98a73455aba89ad5a3b3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-04 09:39:08 +1200

    wadA

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 450.00
 const JUMP_VELOCITY = -750.00
 
-var start_position = Vector2(70,200)
+var start_position = Vector2(70,50)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 		anim.flip_h = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 	if velocity.y > 0:
@@ -37,7 +37,7 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 #respawn
-	if position.y > 1200.00:
+	if position.y > 2000.00:
 		respawn()
 func respawn():
 	position = start_position
